27b4b3d75e88fbe7b13c6efadd016266ce2ba1ee
sahnazmmmdva <84397620+sahnazmmmdva@users.noreply.github.com>
2022-11-01 17:35:52 +0400

    new file added

---
+++ arraytasks/Program.cs
@@ -0,0 +1,27 @@
+﻿#region task1
+//Ədədlərdən ibarət arrayın ən kiçik elementini tapın
+int[] numbers = {2,4,21,58,1,2,32,4,15};
+int min = numbers[0];
+for (int i=1;i<numbers.Length;i++)
+{
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+Console.WriteLine(min);
+
+#endregion
+#region task2
+//Ədədlərdən ibarət arrayda neçə rəqəm olduğunu ekrana çap edin 
+int[] numbers2 = { 2, 4, 21, 58, 1, 2, 32, 4, 15 };
+int count = 0;
+for (int i = 0; i < numbers2.Length; i++)
+{
+    if (numbers2[i]<=9)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
+#endregion
